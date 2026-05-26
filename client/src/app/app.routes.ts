ca7a42435705e0e2d30e0a8d9e09b01601ef19e3
nico190999 <nicolasandressanchezfranco@gmail.com>
2026-05-26 17:03:19 -0300

    Actualizaciones varias

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {
+        path:"auth/login",
+        loadComponent: () =>import("./auth/login/login")
+    },
+    {
+        path:"auth/registrarse",
+        loadComponent: () =>import("./auth/registro/registro")
+    },
+    {
+        path:"miPerfil",
+        loadComponent: () =>import("./pages/mi-perfil/mi-perfil")
+    },
+    {
+        path:"publicaciones",
+        loadComponent: () =>import("./pages/publicaciones/publicaciones")
+    }
+];
